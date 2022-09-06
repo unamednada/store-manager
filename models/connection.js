2537391e22aabcb46d45fed9624fb0eb739c5d43
unamednada <dias.gustavo.dias@gmail.com>
2022-09-06 11:43:14 -0300

    add port to connection, remove migrate from start script

--- models/connection.js
+++ models/connection.js
@@ -7,6 +7,7 @@ const connection = mysql.createPool({
   user: process.env.MYSQL_USER || 'root',
   password: process.env.MYSQL_PASSWORD || '',
   database: process.env.MYSQL_DB_NAME || 'StoreManager',
+  port: process.env.MYSQL_PORT || 3306,
 });
 
 module.exports = connection;
